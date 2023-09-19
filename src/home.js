83c6bc8ea563b3392ea9bacdda4d5272ce1c2687
mounikamaddi <123763049+mounikamaddi@users.noreply.github.com>
2023-09-18 21:26:14 -0700

    Update home.js
    jsx file commit

--- src/home.js
+++ src/home.js
@@ -1,98 +1,152 @@
-const researchAbstract = () => {
-    const doc = document;
-    const head = doc.createElement('head');
-    
-    const metaCharset = doc.createElement('meta');
-    metaCharset.setAttribute('charset', 'UTF-8');
-    head.appendChild(metaCharset);
-
-    const metaViewport = doc.createElement('meta');
-    metaViewport.setAttribute('name', 'viewport');
-    metaViewport.setAttribute('content', 'width=device-width, initial-scale=1.0');
-    head.appendChild(metaViewport);
-
-    const title = doc.createElement('title');
-    title.textContent = 'Research Abstract on Pre-eclampsia';
-    head.appendChild(title);
-
-    doc.appendChild(head);
-
-    const body = doc.createElement('body');
-
-    const p = doc.createElement('p');
-    p.textContent = 'BMJ 2009 Research';
-    body.appendChild(p);
-
-    const style = doc.createElement('style');
-    style.textContent = `
-        body {
-            font-size: 14px;
-        }
-        .sidebar {
-            float: left;
-            width: 20%;
-            font-size: 0.8rem;
-            padding: 20px;
-        }
-        .content {
-            float: right;
-            width: 75%;
-            padding: 20px;
-        }
-    `;
-    head.appendChild(style);
-
-    const sidebarDiv = doc.createElement('div');
-    sidebarDiv.className = 'sidebar';
-    // Authors' Affiliation
-    const authorsSection = doc.createElement('section');
-    const authorsH4 = doc.createElement('h4');
-    authorsH4.textContent = "Authors' Affiliation";
-    authorsSection.appendChild(authorsH4);
-
-    const address = doc.createElement('address');
-    address.innerHTML = `
-        <strong>1</strong><br>
-        Department of Epidemiology,<br>
-        Harvard School of Public Health,<br>
-        677 Huntington Ave, Boston, MA<br>
-        02115, USA<br><br>
-        <strong>2</strong><br>
-        Department of Medical Epidemiology and Biostatistics<br>
-        and Clinical Epidemiology Unit,<br>
-        Department of Medicine,<br>
-        Karolinska Institute, S-171 77<br>
-        Stockholm, Sweden<br>
-    `;
-    authorsSection.appendChild(address);
-    sidebarDiv.appendChild(authorsSection);
-
-    // ... You'd continue to add each section in a similar fashion ...
-
-    // Append the sidebar to the body
-    body.appendChild(sidebarDiv);
-
-    // Content div
-    const contentDiv = doc.createElement('div');
-    contentDiv.className = 'content';
-
-    // Clinical relevance section
-    const clinicalH1 = doc.createElement('h1');
-    clinicalH1.textContent = 'Clinical relevance';
-    contentDiv.appendChild(clinicalH1);
-
-    const clinicalP = doc.createElement('p');
-    clinicalP.textContent = 'In addition to contributing to our understanding of pre-eclampsia, our findings might influence...';
-    contentDiv.appendChild(clinicalP);
-
-    // ... Continue to add the rest of the content...
-
-    body.appendChild(contentDiv);
-
-    doc.appendChild(body);
-
-    // Return the complete document
-    return doc;
-};
-
-document.body.innerHTML = researchAbstract().body.innerHTML;
+var NewComponent = React.createClass({
+    render: function() {
+      return (
+        <div>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Research Abstract on Pre-eclampsia</title>
+          <p>BMJ 2009 Research</p>
+          <style dangerouslySetInnerHTML={{__html: "\n        body {\n            font-size: 14px;\n        }\n\n        .sidebar {\n            float: left;\n            width: 20%;\n            font-size: 0.8rem;\n            padding: 20px;\n        }\n\n        .content {\n            float: right;\n            width: 75%;\n            padding: 20px;\n        }\n    " }} />
+          <div className="sidebar">
+            <section>
+              <h4>Authors' Affiliation</h4>
+              <address>
+                <strong>1</strong><br />
+                Department of Epidemiology,<br />
+                Harvard School of Public Health,<br />
+                677 Huntington Ave, Boston, MA<br />
+                02115, USA<br />
+                <br />
+                <strong>2</strong><br />
+                Department of Medical Epidemiology and Biostatistics<br />
+                and Clinical Epidemiology Unit,<br />
+                Department of Medicine,<br />
+                Karolinska Institute, S-171 77<br />
+                Stockholm, Sweden<br />
+              </address>
+            </section>
+            <section>
+              <h4>Correspondence</h4>
+              <p>Correspondence to: S Hernández-Díaz <a href="mailto:shernan@hsph.harvard.edu">shernan@hsph.harvard.edu</a></p>
+            </section>
+            <section>
+              <h4>Citation</h4>
+              <p>Cite this as: BMJ 2009;338:b2255</p>
+              <p><a href="https://doi.org/10.1136/bmj.b2255">doi:10.1136/bmj.b2255</a></p>
+            </section>
+          </div>
+          <div className="content">
+            {/* Your previous content goes here */}
+            <h1>Clinical relevance</h1>
+            <p>
+              In addition to contributing to our understanding of pre-eclampsia, our findings might influence the information women are given about the risk of pre-eclampsia in a subsequent pregnancy by providing risk estimates stratified by history...
+            </p>
+            {/* And so on... You can continue pasting the content from before here. */}
+          </div>
+          <section>
+            <h1>ABSTRACT</h1>
+            <h2>Objective</h2>
+            <p>To investigate whether pre-eclampsia is more common in first pregnancies solely because fewer affected women, who presumably have a higher risk of recurrence, go on to have subsequent pregnancies.</p>
+            <h2>Design</h2>
+            <p>Prospective cohort study.</p>
+            <h2>Setting</h2>
+            <p>Swedish Medical Birth Register.</p>
+            <h2>Participants</h2>
+            <p>763,795 primiparous mothers who had their first births in Sweden, 1987-2004.</p>
+            <h2>Main outcome measures</h2>
+            <p>Pre-eclampsia.</p>
+            <h2>Results</h2>
+            <p>The risk of pre-eclampsia was 4.1% in the first pregnancy and 1.7% in later pregnancies overall. However, the risk was 14.7% in the second pregnancy for women who had had pre-eclampsia in their first pregnancy and 31.9% for women who had had pre-eclampsia in the previous two pregnancies...</p>
+            {/* Add the rest of the results content... */}
+            <h2>Conclusions</h2>
+            <p>Having pre-eclampsia in one pregnancy is a poor predictor of subsequent pregnancy but a strong predictor for recurrence of pre-eclampsia in future gestations...</p>
+            {/* Complete the conclusions content... */}
+          </section>
+          {/* ... (previous code from above) ... */}
+          <section>
+            <h1>METHODS</h1>
+            <h2>Study population</h2>
+            <p>The Swedish Medical Birth Register is a population based registry that includes more than 98% of all births in Sweden.<sup>6</sup> Starting with the first antenatal visit, normally at 8-12 weeks’ gestation, information is prospectively collected on maternal demographic data, reproductive history, pregnancy, and birth characteristics. Such information is recorded on the standardised antenatal, obstetric, and neonatal records, and copies of these records are forwarded to the birth register, where the information is computerised.</p>
+          </section>
+          {/* ... (previous code from above) ... */}
+          <section>
+            <h2>Outcome Definition and Ascertainment</h2>
+            <p>The diagnoses are noted by a doctor at the time of discharge from the hospital, using ICD-9 and ICD-10 (international classification of diseases, 9th and 10th revisions). Pre-eclampsia is defined as a diastolic blood pressure of at least 90 mm Hg accompanied by proteinuria (≥0.3 g/day or ≥1 on a urine dipstick) (ICD-9 codes 642 E-F and ICD-10 code O14). Eclampsia is defined as the occurrence of pre-eclampsia with seizures (ICD-9 code 642 G and ICD-10 code O15). We included pre-eclamptic and eclamptic pregnancies in our analysis. The quality of the diagnosis of pre-eclampsia has previously been validated: of 148 pregnancies coded as pre-eclampsia in the birth register, 137 (93%) had pre-eclampsia according to the individual records.<sup>7</sup> Early and late onset pre-eclampsia have different heritability, risk factors, clinical manifestations, and prognosis.<sup>8-13</sup> As information on timing of onset was not available through the register, we used preterm delivery as a proxy for early onset and severity. Pre-eclampsia was considered “severe” when delivery occurred before 34 completed gestational weeks.</p>
+            <h2>Statistical Analysis</h2>
+            <p>... [similarly formatted content for this section] ...</p>
+            <h2>RESULTS</h2>
+            <p>The risk of pre-eclampsia in any pregnancy was 3.0%; ... [continue with the rest of the content for this section]</p>
+          </section>
+          {/* ... (previous code from above) ... */}
+          <section>
+            <h2>Impact of Confounders</h2>
+            <p>After having a first pregnancy with pre-eclampsia, the adjusted odds ratio ... [continue with the rest of the content for this section]</p>
+            <h2>Impact of Multiple Gestations</h2>
+            <p>... [similarly formatted content for this section] ...</p>
+            <h2>Impact of Temporal Trends</h2>
+            <p>... [similarly formatted content for this section] ...</p>
+            <h2>Impact of Outcome Definition</h2>
+            <p>... [similarly formatted content for this section] ...</p>
+            <h2>DISCUSSION</h2>
+            <p>In this large prospective cohort study, the incidence of pre-eclampsia ... [continue with the rest of the content for this section]</p>
+          </section>
+          {/* ... (previous code) ... */}
+          <section>
+            <h2>Multiple Gestations</h2>
+            <p>Multiple gestations were associated with ... [continue with the rest of the content for this section] ...</p>
+            <h2>Temporal Trends</h2>
+            <p>The observed slight rise in the risk ... [continue with the rest of the content for this section] ...</p>
+            <h2>More than one condition under the same term</h2>
+            <p>Early and late onset pre-eclampsia differ ... [continue with the rest of the content for this section] ...</p>
+            <h2>Interpretation of Findings</h2>
+            <p>Our findings support the idea of two distinct entities ... [continue with the rest of the content for this section] ...</p>
+          </section>
+          {/* ... (previous code) ... */}
+          <section>
+            <h2>Clinical Relevance</h2>
+            <p>In addition to contributing to our understanding of pre-eclampsia, ... [continue with the rest of the content for this section] ...</p>
+            <h2>Strengths and Weaknesses of the Study</h2>
+            <p>Our study is based on a large, prospective, population-based cohort. ... [continue with the rest of the content for this section] ...</p>
+            <h2>Conclusion</h2>
+            <p>Having pre-eclampsia in one pregnancy is ... [continue with the rest of the content for this section] ...</p>
+          </section>
+          {/* ... (previous code) ... */}
+          <section>
+            <h2>Acknowledgements</h2>
+            <p>We thank Miguel Hernan for his useful comments.</p>
+            <h2>Contributors</h2>
+            <p>All authors jointly designed the hypothesis, analysed and interpreted the data, and wrote the paper. SHD is guarantor.</p>
+            <h2>Funding</h2>
+            <p>This research received no specific grant from any funding agency in the public, commercial, or not-for-profit sectors. The authors had full access to all the data in the study and had the final responsibility for the decision to submit for publication.</p>
+            <h2>Competing Interests</h2>
+            <p>None declared.</p>
+            <h2>Ethical Approval</h2>
+            <p>The study was approved by the research ethic committee at Karolinska Institute, Sweden (No 4863/2005).</p>
+            <h2>References</h2>
+            <ol>
+              <li>Luo Z, An N, Xu H, ...</li>
+              <li>Saftlas AF, Levine R, ...</li>
+              {/* ... Add all other references similarly ... */}
+              <li>Duckitt K, Harrington D. ...</li>
+              <li>Hernández-Díaz S, Werler M, Mitchell A. ...</li>
+            </ol>
+            <h2>Accepted Date</h2>
+            <p>Accepted: 26 February 2009</p>
+            <h2>Key Takeaways</h2>
+            <h3>What is already known on this topic</h3>
+            <p>The risk of pre-eclampsia is higher in first pregnancies (4.1%) than in subsequent ones (1.7%).</p>
+            <h3>What this study adds</h3>
+            <ul>
+              <li>The lower overall risk of pre-eclampsia among parous women is not explained by fewer pregnancies among women who experienced pre-eclampsia in a previous gestation.</li>
+              <li>The risk of recurrence is around 15% for women who had pre-eclampsia in one previous pregnancy and around 30% when two consecutive previous pregnancies were affected.</li>
+              <li>Recurrence is higher for pre-eclampsias associated with very preterm delivery.</li>
+            </ul>
+          </section>
+          <footer>
+            <p>RESEARCH</p>
+            <p>BMJ | ONLINE FIRST | bmj.com</p>
+          </footer>
+        </div>
+      );
+    }
+  });
